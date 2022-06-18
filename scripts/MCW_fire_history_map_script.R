84871369d506dfeef27d1a3cc15e9a0c262ed331
Angeline Emmott <angeline.emmott@gmail.com>
2022-06-17 17:45:17 -0700

    adding to fire history and watershed scripts

--- scripts/MCW_fire_history_map_script.R
+++ scripts/MCW_fire_history_map_script.R
@@ -3,7 +3,7 @@ library(sf)
 library(ggplot2)
 
 #Layer 1: island coastline
-islcoast <- st_read("spatial_data/vector/Shp_files/Island")
+islcoast <- st_read("spatial_data/vectors/Shp_files/Island")
 
 #Layer 2: MCW
 MCW <- st_read("spatial_data/vectors/Shp_files/MCW")
@@ -15,20 +15,13 @@ h_fires <- st_read("spatial_data/vectors/Shp_files/Historic_fires")
 #changing FIRE_YEAR numeric attribute to character:
 h_fires$FIRE_YEAR <- as.character(h_fires$FIRE_YEAR)
 
-ggplot() +
-  geom_sf(data = islcoast, color ="black", fill = "NA") +
-  geom_sf(data = h_fires, mapping = aes(fill = FIRE_YEAR), fill = "red") +
-  geom_sf_text(data = h_fires, aes(label = FIRE_YEAR),check_overlap = TRUE) + 
-  geom_sf(data = MCW, color = "royalblue4", fill = "NA") +
-  ggtitle("Fire History Reference Map")
-coord_sf()
-
 #Map clipped to MXCW:
 ggplot() +
+  geom_sf(data = islcoast, color ="black", fill = "beige") +
   geom_sf(data = h_fires, mapping = aes(fill = FIRE_YEAR)) +
-  scale_fill_gradient(low = "red", high = "orange") +
   geom_sf_text(data = h_fires, aes(label = FIRE_YEAR), check_overlap = TRUE) + 
   geom_sf(data = MCW, color = "royalblue4", fill = "NA") +
   geom_sf(data = CAD_MCW$geometry, fill = "NA") +
-  coord_sf(xlim = c(457989.4, 463653.3), ylim = c(5405057, 5411462), expand = FALSE)
+  coord_sf(xlim = c(457989.4, 463653.3), ylim = c(5405057, 5411462), expand = FALSE) +
+  ggtitle("Fire History Reference Map")
 
--- scripts/MXC_watershed_map_script.R
+++ scripts/MXC_watershed_map_script.R
@@ -2,36 +2,32 @@
 library(sf)
 library(ggplot2)
 
-#layer 1: CAD_MCW
+#Layer 1: island coast
+islcoast <- st_read("spatial_data/vectors/Shp_files/Island")
+
+#layer 2: CAD_MCW
 CAD_MCW <- st_read("spatial_data/vectors/Shp_files/MXCK_CAD_clipped_MXCW")
 
-#Layer 2: MCW
+#Layer 3: MCW
 MCW <- st_read("spatial_data/vectors/Shp_files/MCW")
 
-#Layer 3: waterbodies 
+#Layer 4: waterbodies 
 waterbodies <- st_read("spatial_data/vectors/Shp_files/RAR_water_bodies")
 
-#Layer 4: watercourses
+#Layer 5: watercourses
 watercourses <- st_read("spatial_data/vectors/Shp_files/RAR_water_courses")
 
-#Layer 5: watersheds
-watersheds <- st_read("spatial_data/vectors/Shp_files/Watershed_CRD")watersheds <- st_read("spatial_data/vectors/Shp_files/Watershed_CRD")
-
-#Layer 6: streams 
-streams <- st_read("spatial_data/vectors/Shp_files/Streams")
-
-#layer 7: wetlands
-wetlands <- st_read("spatial_data/vectors/Shp_files/Wetlands")
+#Layer 6: watersheds
+watersheds <- st_read("spatial_data/vectors/Shp_files/Watershed_CRD")
 
 #Mapping using ggplot2:
 
 ggplot() +
-  geom_sf(data = CAD_MCW) +
+  geom_sf(data = islcoast, color ="black", fill = "beige") +
   geom_sf(data = MCW, color = "black", fill = NA) +
-  geom_sf(data = watersheds, color = "blue2", fill = NA) +
-  geom_sf(data = waterbodies, fill = "dodgerblue1") +
-  geom_sf(data = watercourses, fill = "royalblue3") +
-  geom_sf(data = streams) +
-  geom_sf(data = wetlands) +
+  geom_sf(data = CAD_MCW, fill = NA) +
+  geom_sf(data = watersheds, color = "blue2", fill = "NA") +
+  geom_sf(data = waterbodies, color = "dodgerblue1", fill = "dodgerblue1") +
+  geom_sf(data = watercourses, color = "royalblue4", fill = "royalblue4") +
   coord_sf(xlim = c(457989.4, 463653.3), ylim = c(5405057, 5411462), expand = FALSE) +
   ggtitle("Maxwell Creek Watershed Map")
