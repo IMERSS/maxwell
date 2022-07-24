37a96949cd71e42770549f1cf457be1aa29a730c
Angeline Emmott <angeline.emmott@gmail.com>
2022-07-24 16:11:40 -0700

    adding legend to land-use history reference map script and updating to R document

--- scripts/MCW_land_use_map_script.R
+++ scripts/MCW_land_use_map_script.R
@@ -1,6 +1,7 @@
 ##MCW land use history map:
 library(sf)
 library(ggplot2)
+library(dplyr)
 
 #Layer 1: island coastline
 islcoast <- st_read("spatial_data/vectors/Shp_files/Island")
@@ -14,17 +15,24 @@ PA_MCW <- st_read("spatial_data/vectors/Shp_files/IT_protected_areas_clipped")
 #Layer 3: Logging roads and trails
 Lroads <- st_read("spatial_data/vectors/Shp_files/Logging_roads_trails")
 
-#Layer 4: Roads
-roads <- st_read("spatial_data/vectors/Shp_files/DataBC_roads")
+Trails <- Lroads %>%
+  filter(Lroads$Type == "trail")
+nrow(Trails)
 
-#Layer 5: Historic fires
+Roads <- Lroads %>%
+  filter(Lroads$Type == "road")
+nrow(Roads)
+
+#Layer 4: Historic fires
 h_fires <- st_read("spatial_data/vectors/Shp_files/Historic_fires")
 
 ggplot() +
   geom_sf(data = CAD_MCW) +
-  geom_sf(data = PA_MCW, mapping = aes(fill = TYPE_2), color ="black", fill = "NA") +
-  geom_sf(data = h_fires, mapping = aes(fill = FIRE_YEAR)) +
-  geom_sf(data = Lroads, fill = "green4", color = "green4") +
-  geom_sf(data = roads) +
+  geom_sf(data = PA_MCW, color ="black", fill = "NA") +
+  geom_sf(data = h_fires, color ="grey2", fill = "red1") +
+  geom_sf(data = Trails, color = "green4") +
+  geom_sf(data = Roads, color = "orange1") +
+  geom_point(aes(x = NA, y = NA, color ="Legend")) + 
+  scale_color_manual(name = "Legend", breaks =c("Trails", "Logging Roads", "Historic fires"), values = c("Trails" = "green4", "Logging Roads" = "orange1", "Historic fires" = "red1")) +
   coord_sf(xlim = c(458938.9, 462136.9), ylim = c(5406261, 5409918)) + 
   ggtitle("Land-use History Reference Map")
