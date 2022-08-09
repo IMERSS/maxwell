f43ff1f514be80128e3d4a9943b21c177a904285
Andrew Simon <biodiversity.galiano@gmail.com>
2022-08-09 09:15:37 -0700

    updated watershed mapping; fixed error in polygons by suppressing MZ dimensions

--- scripts/MXC_watershed_map_script.R
+++ scripts/MXC_watershed_map_script.R
@@ -10,30 +10,32 @@ lat_lon <- function (data) {
 #Layer 1: island coast
 islcoast <- st_read("spatial_data/vectors/Shp_files/Island", quiet=TRUE)
 
-#layer 2: CAD_MCW
-CAD_MCW <- st_read("spatial_data/vectors/Shp_files/MXCK_CAD_clipped_MXCW", quiet=TRUE)
-
-#Layer 3: MCW
+#Layer 2: MCW
 MCW <- st_read("spatial_data/vectors/Shp_files/MCW", quiet=TRUE)
 
-#Layer 4: waterbodies 
+# drop Z and M dimensions from MCW
+MCW <- st_zm(MCW, drop = T, what = "ZM")
+
+#Layer 3: waterbodies 
 waterbodies <- st_read("spatial_data/vectors/Shp_files/RAR_water_bodies", quiet=TRUE)
 
-#Layer 5: watercourses
+#Layer 4: watercourses
 watercourses <- st_read("spatial_data/vectors/Shp_files/RAR_water_courses", quiet=TRUE)
 
-#Layer 6: watersheds
+#Layer 5: watersheds
 watersheds <- st_read("spatial_data/vectors/Shp_files/Watershed_CRD", quiet=TRUE)
+
+# drop Z and M dimensions from watersheds
+watersheds <- st_zm(watersheds, drop = T, what = "ZM")
 
 #Mapping using ggplot2:
 
 p <- ggplot() +
   geom_sf(data = islcoast, color ="black", fill = "beige") +
   geom_sf(data = MCW, color = "black", fill = NA) +
-  geom_sf(data = CAD_MCW, fill = NA) +
   geom_sf(data = watersheds, color = "blue2", fill = "NA") +
   geom_sf(data = waterbodies, color = "dodgerblue1", fill = "dodgerblue1") +
-  geom_sf(data = watercourses, color = "royalblue4", fill = "royalblue4") +
+  geom_sf(data = watercourses, color = "royalblue4", fill = "NA") +
   coord_sf(xlim = c(457989.4, 463653.3), ylim = c(5405057, 5411462), expand = FALSE) +
   ggtitle("Maxwell Creek Watershed Map")
 
@@ -44,9 +46,8 @@ print(p)
 WatershedMap <- leaflet() %>%
   addTiles(options = providerTileOptions(opacity = 0.5)) %>%
   addPolygons(data = lat_lon(islcoast), color = "black", weight = 1.5, fillOpacity = 0) %>%
-#  addPolygons(data = lat_lon(MCW), color = "black", weight = 1.5, fillColor = NA) %>%
-#  addPolygons(data = lat_lon(CAD_MCW), color = "black", weight = 1.5, fillOpacity = 0.8) %>%
-#  addPolylines(data = lat_lon(watersheds), color = "#0000EE", weight = 2) %>%
+  addPolygons(data = lat_lon(MCW), color = "black", weight = 1.5, fillColor = NA) %>%
+  addPolylines(data = lat_lon(watersheds), color = "#0000EE", weight = 2) %>%
   addPolygons(data = lat_lon(watercourses), weight = 1.5, fillOpacity = 0.8, fillColor = "royalblue") %>%
   addPolygons(data = lat_lon(waterbodies), weight = 1.5, fillOpacity = 0.8, fillColor = "dodgerblue")  
   
