ccba498b0e36ce4d2f652d95851e9a7d81b6cfd9
Andrew Simon <biodiversity.galiano@gmail.com>
2022-08-09 08:15:51 -0700

    removed CAD mapping from fire history map

--- scripts/MCW_fire_history_map_script.R
+++ scripts/MCW_fire_history_map_script.R
@@ -17,9 +17,6 @@ MCW <- st_read("spatial_data/vectors/Shp_files/MCW", quiet = TRUE)
 #Layer #3: historic fires
 h_fires <- st_read("spatial_data/vectors/Shp_files/Historic_fires", quiet = TRUE)
 
-#Layer 4: CAD_MCW
-CAD_MCW <- st_read("spatial_data/vectors/Shp_files/MXCK_CAD_clipped_MXCW", quiet = TRUE)
-
 ##Mapping based on fire year: 
 #changing FIRE_YEAR numeric attribute to character:
 h_fires$FIRE_YEAR <- as.character(h_fires$FIRE_YEAR)
@@ -29,7 +26,6 @@ p <- ggplot() +
   geom_sf(data = islcoast, color ="black", fill = "beige") +
   geom_sf(data = h_fires, mapping = aes(fill = FIRE_YEAR)) +
   geom_sf(data = MCW, color = "royalblue4", fill = "NA") +
-  geom_sf(data = CAD_MCW$geometry, fill = "NA") +
   geom_sf_text(data = h_fires, aes(label = FIRE_YEAR), check_overlap = TRUE) +
   theme(legend.position = "none") +
   coord_sf(xlim = c(457989.4, 463653.3), ylim = c(5405057, 5411462), expand = FALSE) +
@@ -45,7 +41,6 @@ FirehistMap <- leaflet() %>%
   addPolygons(data = lat_lon(islcoast), color = "black", weight = 1.5, fillOpacity = 0, fillColor = NA) %>%
   addPolygons(data = lat_lon(h_fires), color = "#050505", weight = 2, fillOpacity = 0.7, fillColor = "red") %>%
 #  addPolygons(data = lat_lon(MCW), color = "royalblue4", weight = 2, fillOpacity = 0.7, fillColor = "royalblue4") %>%
-  addPolygons(data = lat_lon(CAD_MCW), color = "black", weight = 2, fillOpacity = 0, fillColor = NA) %>%
   fitBounds(-123.564, 48.802, -123.516, 48.855) %>%
   addLegend(position = "topright", labels = "Fire Years", colors = "red")
 
