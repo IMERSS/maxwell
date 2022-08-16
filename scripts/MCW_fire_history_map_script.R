4740e2785891a089cbb7683062c4e07219c936c5
Angeline Emmott <angeline.emmott@gmail.com>
2022-08-16 11:13:42 -0700

    adding label markers to fire history map

--- scripts/MCW_fire_history_map_script.R
+++ scripts/MCW_fire_history_map_script.R
@@ -24,9 +24,13 @@ h_fires <- st_read("spatial_data/vectors/Shp_files/Historic_fires", quiet = TRUE
 #changing FIRE_YEAR numeric attribute to character:
 h_fires$FIRE_YEAR <- as.character(h_fires$FIRE_YEAR)
 
+#sf coordinates at centroid
+Coord_h_fires <- lat_lon(h_fires)
+sf::st_coordinates(st_centroid(Coord_h_fires$geometry))
+
 #Map clipped to MXCW:
 p <- ggplot() +
-#  geom_sf(data = islcoast, color ="black", fill = "beige") +
+  geom_sf(data = islcoast, color ="black", fill = "beige") +
   geom_sf(data = h_fires, mapping = aes(fill = FIRE_YEAR)) +
   geom_sf(data = MCW, color = "royalblue4", fill = "NA") +
   geom_sf_text(data = h_fires, aes(label = FIRE_YEAR), check_overlap = TRUE) +
@@ -41,9 +45,13 @@ print(p)
 
 FirehistMap <- leaflet() %>%
   addTiles(options = providerTileOptions(opacity = 0.5)) %>%
-#  addPolygons(data = lat_lon(islcoast), color = "black", weight = 1.5, fillOpacity = 0, fillColor = NA) %>%
+  addPolygons(data = lat_lon(islcoast), color = "black", weight = 1.5, fillOpacity = 0, fillColor = NA) %>%
   addPolygons(data = lat_lon(h_fires), color = "#050505", weight = 2, fillOpacity = 0.7, fillColor = "red") %>%
   addPolygons(data = lat_lon(MCW), color = "blue", weight = 2, fillOpacity = 0) %>%
+  addLabelOnlyMarkers(data = lat_lon(h_fires), lng = -123.5670, lat = 48.84805, label = "1934", labelOptions = labelOptions(noHide = TRUE, direction = 'middle', textOnly = TRUE)) %>%
+  addLabelOnlyMarkers(data = lat_lon(h_fires), lng = -123.5519, lat = 48.85045, label = "1935", labelOptions = labelOptions(noHide = TRUE, direction = 'middle', textOnly = TRUE)) %>%
+  addLabelOnlyMarkers(data = lat_lon(h_fires), lng = -123.5230, lat = 48.82397 , label = "1935", labelOptions = labelOptions(noHide = TRUE, direction = 'middle', textOnly = TRUE)) %>%
+  addLabelOnlyMarkers(data = lat_lon(h_fires), lng = -123.5207, lat = 48.81398, label = "1942", labelOptions = labelOptions(noHide = TRUE, direction = 'middle', textOnly = TRUE)) %>%
   fitBounds(-123.564, 48.802, -123.516, 48.855) %>%
   addLegend(position = "topright", labels = "Fire Years", colors = "red")
 
